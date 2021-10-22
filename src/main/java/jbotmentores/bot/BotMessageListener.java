032b6febd2bcc5624b0d260649d731b4431109fe
gabriela moraes <gabriela.omoraes@gmail.com>
2021-10-21 22:05:35 -0300

    Refactors listener to a separate file

---
+++ src/main/java/jbotmentores/bot/BotMessageListener.java
@@ -0,0 +1,46 @@
+package jbotmentores.bot;
+
+import jbotmentores.model.JBotData;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+class BotMessageListener extends ListenerAdapter {
+
+    private JBotData jBotData;
+
+    public BotMessageListener(JBotData jBotData) {
+        this.jBotData = jBotData;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandEvent event) {
+        if (event.getSubcommandName() == "list") {
+            if (event.getOptions().contains("dia")) {
+                listMentorsByDay(event, event.getOption("dia").getAsLong());
+            }
+
+            if (event.getOptions().contains("skill")) {
+                listMentorsBySkill(event, event.getOption("skill").getAsString());
+            }
+
+            if (event.getOptions().contains("user")) {
+                listUser(event, event.getOption("user").getAsUser());
+            }
+        }
+    }
+
+    private void listUser(SlashCommandEvent event, User user) {
+
+        event.reply("Entendi: " + event.getCommandPath() + " " + event.getSubcommandGroup() + " " + event.getOptions())
+                .setEphemeral(true)
+                .queue();
+
+    }
+
+    private void listMentorsBySkill(SlashCommandEvent event, String skill) {
+    }
+
+    private void listMentorsByDay(SlashCommandEvent event, Long dia) {
+    }
+}
