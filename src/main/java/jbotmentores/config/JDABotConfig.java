4db7ca2387efa8d6e2af6591f0f3712c642486c8
Maximillian Arruda <dearrudam@gmail.com>
2021-10-21 21:53:20 -0300

    Adding more intelligence on it

--- src/main/java/jbotmentores/config/JDABotConfig.java
+++ src/main/java/jbotmentores/config/JDABotConfig.java
@@ -2,16 +2,25 @@ package jbotmentores.config;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.IEventManager;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +29,8 @@ import org.springframework.core.env.Environment;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.security.auth.login.LoginException;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class JDABotConfig {
@@ -34,24 +45,46 @@ public class JDABotConfig {
 
     @PostConstruct
     void start() throws LoginException, InterruptedException {
-        JDABuilder builder = JDABuilder.createDefault(environment.getProperty("BOT_TOKEN"));
+        JDABuilder builder = JDABuilder.createLight(environment.getProperty("BOT_TOKEN"), Collections.emptyList());
         // Disable parts of the cache
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
-        // Enable the bulk delete event
-        builder.setBulkDeleteSplittingEnabled(false);
-        // Disable compression (not recommended)
-        builder.setCompression(Compression.NONE);
+//        // Enable the bulk delete event
+//        builder.setBulkDeleteSplittingEnabled(false);
+//        // Disable compression (not recommended)
+//        builder.setCompression(Compression.NONE);
 
-        configureMemoryUsage(builder);
+//        configureMemoryUsage(builder);
 
         builder.addEventListeners(new ReadyListener());
         builder.addEventListeners(new BotMessageListener());
 
+        builder.setActivity(Activity.playing("Type /ping"));
+
         this.jda = builder.build();
 
-        // optionally block until JDA is ready
+        updateCommands(this.jda);
+
         jda.awaitReady();
 
+    }
+
+    private void updateCommands(JDA jda) {
+        // These commands take up to an hour to be activated after creation/update/delete
+        CommandListUpdateAction commands = jda.updateCommands();
+
+        // Moderation commands with required options
+        commands.addCommands(
+                new CommandData("mentor", "Listar mentores disponiveis por data")
+                        .addSubcommands(
+                                new SubcommandData("list", "lista mentores")
+                                        .addOptions(new OptionData(OptionType.INTEGER, "dia", "Informe o dia (22 | 23 | 24)"))
+                                        .addOptions(new OptionData(OptionType.STRING, "skill", "Informe o skill"))
+                                        .addOptions(new OptionData(OptionType.USER, "user", "Mencione o mentor"))
+                        )
+                         // This command requires a parameter
+        );
+        // Send the new set of commands to discord, this will override any existing global commands with the new set provided here
+        commands.queue();
     }
 
     @PreDestroy
@@ -65,7 +98,7 @@ public class JDABotConfig {
         builder.disableCache(CacheFlag.ACTIVITY);
 
         // Disable member chunking on startup
-//        builder.setChunkingFilter(ChunkingFilter.NONE);
+        // builder.setChunkingFilter(ChunkingFilter.NONE);
 
         // Disable presence updates and typing events
         builder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
@@ -81,21 +114,24 @@ public class JDABotConfig {
         public void onEvent(GenericEvent event) {
             if (event instanceof ReadyEvent)
                 System.out.println("API is ready!");
+
         }
     }
 
     static class BotMessageListener extends ListenerAdapter {
+
         @Override
-        public void onMessageReceived(MessageReceivedEvent event) {
-            Message msg = event.getMessage();
-            if (msg.getContentRaw().equals("!ping")) {
-                MessageChannel channel = event.getChannel();
-                long time = System.currentTimeMillis();
-                channel.sendMessage("Pong!") /* => RestAction<Message> */
-                        .queue(response /* => Message */ -> {
-                            response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                        });
-            }
+        public void onSlashCommand(SlashCommandEvent event) {
+            System.out.println(event);
+            event.reply("Entendi: " + event.getCommandPath() + " " + event.getSubcommandGroup() + " " + event.getOptions())
+                    .setEphemeral(true)
+                    .queue();
+//            if (!event.getName().equals("ping")) return; // make sure we handle the right command
+//            long time = System.currentTimeMillis();
+//            event.reply("Pong!").setEphemeral(true) // reply or acknowledge
+//                    .flatMap(v ->
+//                            event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
+//                    ).queue(); // Queue both reply and edit
         }
     }
 }
